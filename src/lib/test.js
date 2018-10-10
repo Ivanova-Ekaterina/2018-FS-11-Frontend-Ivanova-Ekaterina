454c127c666f99a2d5b18fd1e15206bdab332639
Ekaterina Ivanova <42977038+Ivanova-Ekaterina@users.noreply.github.com>
2018-10-10 22:30:30 +0300

    Домашнее задание 1
    Вынесла стили в css
    Добавила eslint(при запуске выдает ошибки  'document' is not defined')

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,9 +1,7 @@
 const say = function (name) {
   const elem = document.createElement('div');
-  elem.style.backgroundColor = 'red';
-  elem.style.height = '100px';
-  elem.style.fontSize = '100px';
-  elem.style.width = '500px';
+  elem.className = 'box';
+  elem.id = 'test';
   const text = document.createTextNode(`Hello ${name}`);
   document.body.appendChild(elem);
   elem.appendChild(text);
